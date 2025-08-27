7cb6622a71920f85fc14ff372714fadb0ad44637
Kareem <68719065+KareemHafidzh@users.noreply.github.com>
2025-08-27 21:49:58 +0700

    Add files via upload

---
+++ FlashChat/Flash Chat/Controllers/WelcomeViewController.swift
@@ -0,0 +1,46 @@
+//
+//  WelcomeViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Angela Yu on 21/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+//import CLTypingLabel
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //text animation with cocoapods library
+//        titleLabel.text = "⚡️FlashChat"
+        
+        //text animation manually
+        titleLabel.text = ""
+        var charIndex = 0
+        let titleText = K.appName
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(charIndex), repeats: false) { timer in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
+       
+    }
+    
+
+}
---
+++ FlashChat/Flash Chat/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  FlashChat_iOS13
+//
+//  Created by Kareem Abdul Hafidzh on 25/08/25.
+//  Copyright © 2025 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message{
+    let sender: String //email person
+    let body: String
+}
